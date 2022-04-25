a94bd0ca23e633a2fa801dc0e954b835d75063a6
tanianasrin1 <tasrineva169@gmail.com>
2022-04-25 23:55:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require( 'express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use (cors());
+app.use (express.json());
+
+app.get ('/', (req, res)=>{
+    res.send('John is running and wait for Ema');
+});
+
+app.listen(port, () => {
+    console.log('John is running on port', port)
+})
